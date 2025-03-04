d23a8068db79c133847615ebfff9da230f327f15
OmarAyman12 <omaraymanahmed@gmail.com>
2025-03-04 15:04:29 +0200

    Modified file locations and added Navbar to layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,11 @@
-'use client';
+"use client";
 
-import ThreeCarousel from '@/components/ThreeCarousel';
+import ThreeCarousel from "./home/ThreeCarousel";
 
 export default function Home() {
   return (
-    <main style={{ height: '200vh', background: '#111' }}>
-      <h1 style={{ color: '#fff', textAlign: 'center', padding: '50px 0' }}>
-        Tujyro Portfolio
-      </h1>
-      <ThreeCarousel />
+    <main style={{ height: "200vh", background: "#111" }}>
+      <ThreeCarousel/>
     </main>
   );
 }
---
+++ components/Navbar/navbar.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Navbar() {
+  return (
+    <div className="text-center fixed bg-white w-full">Tujiro portfolio</div>
+  );
+}
+
+export default Navbar;

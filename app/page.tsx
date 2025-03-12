460189b3e0da8b56e86f94bb79966d52153a2635
Marwan Mostafa <marawan206@gmail.com>
2025-03-12 05:37:50 +0200

    Fixed Dark mode for main page

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Hero from "@/components/Hero";
 
 export default function Home() {
   return (
-    <main style={{ height: "200vh", background: "#111" }}>
+    <main style={{ height: "200vh" }}>
       <Hero/>
     </main>
   );
